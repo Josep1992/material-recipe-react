3c4e0515a76b5e7dc4a5196fffa784a9d8519fa4
Josep <coding1992@outlook.com>
2018-08-24 15:16:35 -0400

    Content is rendering on the page

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Card from '@material-ui/core/Card';
+import { Link } from 'react-router-dom';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 import { config } from '../../config';
 
 class Recipe extends Component {
@@ -31,16 +33,31 @@ class Recipe extends Component {
       source_url,
       publisher,
       social_rank,
-    } = this.state;
-    console.log(this.state.recipe);
+    } = this.state.recipe;
+
     return (
-      <Card>
-        {this.state.recipe.length !== 0 ? (
-          <CardMedia image={image_url} title={title} />
-        ) : (
-          console.log(this.state.recipe)
+      <Fragment>
+        {this.state.recipe.length !== 0 && (
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image={image_url}
+              title={title}
+            />
+            <CardContent>
+              <h1>{title}</h1>
+              <span>{recipe_id}</span>
+              <br />
+              <CardActions>
+                <Button>
+                  <Link to={{ pathname: publisher }}>Link Here</Link>
+                </Button>
+              </CardActions>
+            </CardContent>
+          </Card>
         )}
-      </Card>
+      </Fragment>
     );
   }
 }
