7705248a423460f691418e4ec6c6ddd06ae1ebb9
Josep <coding1992@outlook.com>
2018-08-26 13:12:18 -0400

    AppBar renders in every route

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
 
 import App from '../../App';
 import Recipe from '../Recipe/Recipe';
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/recipe/:id" component={Recipe} />
-    </Switch>
+    <Fragment>
+      <AppBar position="static">
+        <h1>Recipe Search</h1>
+      </AppBar>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/recipe/:id" component={Recipe} />
+      </Switch>
+    </Fragment>
   </BrowserRouter>
 );
 
