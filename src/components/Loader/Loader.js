bbf575f61ef516d696c7ffc2509950c7a286f047
Josep <coding1992@outlook.com>
2018-08-26 00:46:55 -0400

    Loader styles, centered Image

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import GIF from '../../assets/Spinner.gif';
 
-class Loader extends Component {
-  render() {
-    return <div>Loading...</div>;
-  }
-}
+const Loader = () => (
+  <LoaderWrappper>
+    <Image src={GIF} alt="Loader" />
+  </LoaderWrappper>
+);
+
+const LoaderWrappper = styled.div`
+  margin: 0 auto;
+  width: 300px;
+  height: 300px;
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+`;
 
 export default Loader;
