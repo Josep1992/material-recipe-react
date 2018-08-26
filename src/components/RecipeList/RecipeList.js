a0b59557bcfde9e67ed17f521153247c76f18ae1
Josep <coding1992@outlook.com>
2018-08-26 00:46:44 -0400

    Styles

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,13 +1,30 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import { withStyles } from '@material-ui/core/styles';
 
-const RecipesList = ({ recipes }) => (
-  <Fragment>
+const styles = () => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+});
+
+const RecipesList = ({ recipes, classes }) => (
+  <div className={classes.root}>
     {recipes.length !== 0 ? (
-      <GridList cols={4} cellHeight={150}>
+      <GridList cols={3} cellHeight={200} classes={classes.gridList}>
         {recipes.map(recipe => (
           <GridListTile key={recipe.recipe_id} recipe={recipe} cols={1}>
             <img src={recipe.image_url} alt={recipe.title} />
@@ -32,6 +49,6 @@ const RecipesList = ({ recipes }) => (
     ) : (
       console.log(recipes)
     )}
-  </Fragment>
+  </div>
 );
-export default RecipesList;
+export default withStyles(styles)(RecipesList);
